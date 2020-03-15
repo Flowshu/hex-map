2c1baa21b1d362019fa77de92a5f8f9893e70fb2
fl0wshu <floowshu@gmail.com>
2020-03-15 21:09:47 +0100

    :wrench: change HexGrid layout

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "babylonjs";
+import { Engine, Scene, FreeCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "babylonjs";
 import { HexCell } from "./src/HexCell";
 import { HexGrid } from "./src/HexGrid";
 
@@ -8,14 +8,14 @@ const engine: Engine = new Engine(canvas, true);
 function createScene(): Scene {
     const scene: Scene = new Scene(engine);
 
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-    camera.setTarget(BABYLON.Vector3.Zero());
+    var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+    camera.setTarget(Vector3.Zero());
     camera.attachControl(canvas, true);
 
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
 
-    var map: HexGrid = new HexGrid(20, 10, 2, scene);
+    var map: HexGrid = new HexGrid(5, 10, 2, scene);
 
     return scene;
 }
